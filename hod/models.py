6e4d863bde7df33545a6bb5ede8d5df2caa7036c
mohitrajane <mhit98@gmail.com>
2019-03-12 18:06:07 +0530

    add models

--- hod/models.py
+++ hod/models.py
@@ -1,9 +1,11 @@
 from django.db import models
 
+class HOD(models.Model):
+    name = models.CharField(max_length=30)
+    branch_id = models.IntegerField()
+
 class Question(models.Model):
     query = models.CharField(max_length=300)
-    branch_id = models.IntegerField()
+    branch_id = models.ForeignKey(HOD,on_delete=models.CASCADE)
     query_date = models.DateField('date asked')
 
-class Departments(models.Model):
-    
