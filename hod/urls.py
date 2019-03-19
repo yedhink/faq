fedc2d94bef740e352dfcd83295790f85219d5ed
mohitrajane <mhit98@gmail.com>
2019-03-19 18:51:03 +0530

    add auth

--- hod/urls.py
+++ hod/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path
+from django.urls import path,include
 
 from . import views
 
 urlpatterns=[
     path('',views.index,name='index'),
     path('success',views.on_submit,name="on_submit"),
-    path('hod/<int:hod_id>/',views.hod_view,name="hod_view")
+    path('hod/<int:hod_id>/',views.hod_view,name="hod_view"),
+    path('accounts/', include('django.contrib.auth.urls')),
 ]
