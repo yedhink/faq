3751b5fe915206bf891fb1281e92f9580f2a14b7
mohitrajane <mhit98@gmail.com>
2019-03-22 18:42:18 +0530

    add email response send

--- hod/urls.py
+++ hod/urls.py
@@ -9,5 +9,6 @@ urlpatterns=[
     path('logout',views.logout,name="logout"),
     path('hod/<int:hod_id>/',views.hod_view,name="hod_view"),
     path('accounts/', include('django.contrib.auth.urls')),
-    path('hod/<int:hod_id>/<int:q_id>/',views.del_item,name="del_item")
+    path('hod/<int:hod_id>/<int:q_id>/',views.del_item,name="del_item"),
+    path('send_query_response',views.send_query_response,name="send_query_response")
 ]
